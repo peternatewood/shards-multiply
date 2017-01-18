c20c91c1db20d0877721d0168cf668e61ebabc45
Peter Wood <pwood@sankyinc.com>
2017-01-18 15:02:46 -0500

    Add round function to round numbers by specified decimal place or 2 places

--- base.js
+++ base.js
@@ -8,6 +8,13 @@ var LEVELS = [
   { l: 0, t: 0, r: 2000, b: 2000 }
 ];
 
+var round = function(num, place) {
+  if (isNaN(num)) return num;
+  var place = place ? place : 2;
+  var mod = Math.pow(10, place);
+  return Math.round(num * mod) / mod;
+}
+
 var gInput = {}
 var gPlayer = {}
 var gScene = {}
