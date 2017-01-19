1f944088fdcc0d10a8fd4b3a97891e3c99024980
Peter Wood <pwood@sankyinc.com>
2017-01-19 14:23:16 -0500

    Skip rendering targets if outside camera's bounds

--- target.js
+++ target.js
@@ -6,17 +6,23 @@ Target = function(x, y) {
 }
 Target.prototype.render = (function(context) {
   function render() {
-    context.beginPath();
-    context.arc(this.x + 2, this.y + 2, 13, 0, Math.PI * 2);
-    context.fillStyle = '#222';
-    context.fill();
-    context.closePath();
+    var x = this.x - gCamera.x;
+    var y = this.y - gCamera.y;
 
-    context.beginPath();
-    context.arc(this.x, this.y, 12, 0, Math.PI * 2);
-    context.fillStyle = 'rgb(' + Math.round(255 * (this.life / 12)) + ',' + Math.round(255 * ((12 - this.life) / 12)) + ',' + 0x22 + ')';
-    context.fill();
-    context.closePath();
+    // Only render if within the camera's bounds
+    if (x + 12 > 0 && x - 12 < SCREEN_WIDTH && y + 12 > 0 && y - 12 < SCREEN_HEIGHT) {
+      context.beginPath();
+      context.arc(x + 2, y + 2, 13, 0, Math.PI * 2);
+      context.fillStyle = '#222';
+      context.fill();
+      context.closePath();
+
+      context.beginPath();
+      context.arc(x, y, 12, 0, Math.PI * 2);
+      context.fillStyle = 'rgb(' + Math.round(255 * (this.life / 12)) + ',' + Math.round(255 * ((12 - this.life) / 12)) + ',' + 0x22 + ')';
+      context.fill();
+      context.closePath();
+    }
   }
   return render;
 })(gRenderer.context);
