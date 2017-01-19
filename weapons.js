a5128ae9e3544643e3dd2bea61689dabb1070d90
Peter Wood <pwood@sankyinc.com>
2017-01-19 14:34:56 -0500

    Update Bolt rendering to use gCamera.isInView

--- weapons.js
+++ weapons.js
@@ -39,35 +39,40 @@ Bolt.prototype.collide = function(other) {
 }
 Bolt.prototype.render = (function(context) {
   function render() {
-    context.beginPath();
-    switch (this.life) {
-      case 4:
-        context.moveTo(this.x + (4 * Math.cos(this.rad)), this.y + (4 * Math.sin(this.rad)));
-        context.lineTo(this.x + (4 * Math.cos(this.rad + (Math.PI / 2))), this.y + (4 * Math.sin(this.rad + (Math.PI / 2))));
-        context.lineTo(this.x + (8 * Math.cos(this.rad + Math.PI)), this.y + (8 * Math.sin(this.rad + Math.PI)));
-        context.lineTo(this.x + (4 * Math.cos(this.rad - (Math.PI / 2))), this.y + (4 * Math.sin(this.rad - (Math.PI / 2))));
-        break;
-      case 3:
-      case 2:
-      case 1:
-        context.moveTo(this.x + (3 * (4 - this.life)), this.y);
-        context.arc(this.x + (3 * (4 - this.life)), this.y, 1 * this.life, 0, Math.PI * 2);
-        context.moveTo(this.x, this.y - (3 * (4 - this.life)));
-        context.arc(this.x, this.y - (3 * (4 - this.life)), 1 * this.life, 0, Math.PI * 2);
-        context.moveTo(this.x - (3 * (4 - this.life)), this.y);
-        context.arc(this.x - (3 * (4 - this.life)), this.y, 1 * this.life, 0, Math.PI * 2);
-        context.moveTo(this.x, this.y + (3 * (4 - this.life)));
-        context.arc(this.x, this.y + (3 * (4 - this.life)), 1 * this.life, 0, Math.PI * 2);
-        break;
+    var x = this.x - gCamera.x;
+    var y = this.y - gCamera.y;
+
+    if (gCamera.isInView(this)) {
+      context.beginPath();
+      switch (this.life) {
+        case 4:
+          context.moveTo(x + (4 * Math.cos(this.rad)), y + (4 * Math.sin(this.rad)));
+          context.lineTo(x + (4 * Math.cos(this.rad + (Math.PI / 2))), y + (4 * Math.sin(this.rad + (Math.PI / 2))));
+          context.lineTo(x + (8 * Math.cos(this.rad + Math.PI)), y + (8 * Math.sin(this.rad + Math.PI)));
+          context.lineTo(x + (4 * Math.cos(this.rad - (Math.PI / 2))), y + (4 * Math.sin(this.rad - (Math.PI / 2))));
+          break;
+        case 3:
+        case 2:
+        case 1:
+          context.moveTo(x + (3 * (4 - this.life)), y);
+          context.arc(x + (3 * (4 - this.life)), y, 1 * this.life, 0, Math.PI * 2);
+          context.moveTo(x, y - (3 * (4 - this.life)));
+          context.arc(x, y - (3 * (4 - this.life)), 1 * this.life, 0, Math.PI * 2);
+          context.moveTo(x - (3 * (4 - this.life)), y);
+          context.arc(x - (3 * (4 - this.life)), y, 1 * this.life, 0, Math.PI * 2);
+          context.moveTo(x, y + (3 * (4 - this.life)));
+          context.arc(x, y + (3 * (4 - this.life)), 1 * this.life, 0, Math.PI * 2);
+          break;
+      }
+      context.closePath();
+
+      context.fillStyle = '#FFF';
+      context.strokeStyle = '#F88';
+      context.lineWidth = 2;
+
+      context.stroke();
+      context.fill();
     }
-    context.closePath();
-
-    context.fillStyle = '#FFF';
-    context.strokeStyle = '#F88';
-    context.lineWidth = 2;
-
-    context.stroke();
-    context.fill();
     if (this.dying) this.life--;
   }
   return render;
