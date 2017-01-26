33f4e2199b69d1a1862bcb079953ec13ea0fccf9
Pete <peternatewood@gmail.com>
2017-01-26 00:26:04 -0500

    Add gAudio function to play individual notes

--- audio.js
+++ audio.js
@@ -31,6 +31,25 @@ gAudio = (function(context) {
       this.eChan1.gain.gain.linearRampToValueAtTime(0.1, now + 0.1);
       this.eChan1.gain.gain.linearRampToValueAtTime(0, now + 0.11);
       this.eChan1.stop(now + 0.11);
+    },
+    playNote: function(chan, note, octave, start, duration) {
+      var base;
+      switch (note) {
+        case 'c'  : base = 16.35; break;
+        case 'db' : base = 17.32; break;
+        case 'd'  : base = 18.35; break;
+        case 'eb' : base = 19.45; break;
+        case 'e'  : base = 20.60; break;
+        case 'f'  : base = 21.83; break;
+        case 'gb' : base = 23.12; break;
+        case 'g'  : base = 24.50; break;
+        case 'ab' : base = 25.96; break;
+        case 'a'  : base = 27.50; break;
+        case 'bb' : base = 29.14; break;
+        case 'b'  : base = 30.87; break;
+      }
+      this['mChan' + chan].start(base * Math.pow(2, octave), start);
+      this['mChan' + chan].stop(context.currentTime + start + duration);
     }
   }
 })(new AudioContext());
