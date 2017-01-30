e1520544d07e7ecec4e65694911c38718df4ab6a
Peter Wood <pwood@sankyinc.com>
2017-01-30 16:07:13 -0500

    Add Shard class with behavior for shard type 0

--- target.js
+++ target.js
@@ -24,3 +24,72 @@ Target.prototype.render = function() {
     }
   }
 }
+
+var SHARD_COLORS = [
+  ['#37D', '#7AF'],
+];
+
+Shard = function(x, y, type) {
+  this.x = x;
+  this.y = y;
+  this.xVel = 0;
+  this.yVel = 0;
+  this.rad = 0;
+  this.type = type;
+  this.speed = Shard.speed(type);
+  this.dying = false;
+
+  return this;
+}
+Shard.speed = function(type) {
+  switch (type) {
+    case 0: return 3;
+  }
+}
+Shard.prototype.collide = function(actor) {
+  return Math.sqrt(Math.pow(actor.x - this.x, 2) + Math.pow(actor.y - this.y, 2)) < actor.size + 12;
+}
+Shard.prototype.update = function() {
+  if (! this.dying) {
+    var xAcc = 0;
+    var yAcc = 0;
+    this.xVel *= FRICTION;
+    this.yVel *= FRICTION;
+    switch (this.type) {
+      case 0:
+        this.rad = Math.atan2(gPlayer.y - this.y, gPlayer.x - this.x);
+        xAcc = Math.cos(this.rad);
+        yAcc = Math.sin(this.rad);
+        break;
+    }
+    this.xVel += xAcc;
+    this.yVel += yAcc;
+
+    if (Math.abs(this.xVel) < 0.01) this.xVel = 0;
+    if (Math.abs(this.yVel) < 0.01) this.yVel = 0;
+
+    this.x = Math.min(Math.min(gCamera.x + SCREEN_WIDTH, gScene.bounds.r) - 20, Math.max(Math.max(gCamera.x, gScene.bounds.l) + 20, this.x + this.xVel * this.speed));
+    this.y = Math.min(Math.min(gCamera.y + SCREEN_HEIGHT, gScene.bounds.b) - 20, Math.max(Math.max(gCamera.y, gScene.bounds.t) + 20, this.y + this.yVel * this.speed));
+  }
+
+  if (this.collide(gPlayer)) this.dying = true;
+}
+Shard.prototype.render = function() {
+  var path = [];
+  switch (this.type) {
+    case 0:
+      path = [
+        ['moveTo', 16 * Math.cos(this.rad), 16 * Math.sin(this.rad)],
+        ['lineTo', 5 * Math.cos(this.rad + (Math.PI / 2)), 5 * Math.sin(this.rad + (Math.PI / 2))],
+        ['lineTo', 2 * Math.cos(this.rad + (Math.PI / 2)), 2 * Math.sin(this.rad + (Math.PI / 2))],
+        ['lineTo', 2 * Math.cos(this.rad + (Math.PI / 2)), 2 * Math.sin(this.rad + (Math.PI / 2))],
+        ['lineTo', 16 * Math.cos(this.rad + Math.PI), 16 * Math.sin(this.rad + Math.PI)],
+        ['lineTo', 5 * Math.cos(this.rad - (Math.PI / 2)), 5 * Math.sin(this.rad - (Math.PI / 2))],
+        ['lineTo', 2 * Math.cos(this.rad - (Math.PI / 2)), 2 * Math.sin(this.rad - (Math.PI / 2))],
+      ];
+      break;
+  }
+  renderPath(path, true, this.x - gCamera.x, this.y - gCamera.y);
+  stroke(SHARD_COLORS[this.type][1], 3);
+  fill(SHARD_COLORS[this.type][0]);
+}
