9f4144ee6c33e14aa5e824b117450083c3296ec9
Peter Wood <pwood@sankyinc.com>
2017-01-30 11:07:15 -0500

    Update Target to accept different types

--- target.js
+++ target.js
@@ -1,8 +1,9 @@
-Target = function(x, y) {
+Target = function(x, y, type) {
   this.x = x;
   this.y = y;
   this.life = 11;
   this.size = 12;
+  this.type = typeof type === 'string' ? type : 'circle';
 
   return this;
 }
@@ -12,10 +13,14 @@ Target.prototype.render = function() {
 
   // Only render if within the camera's bounds
   if (gCamera.isInView(this)) {
-    renderPath([['arc', x + 2, y + 2, 13, 0, Math.PI * 2]]);
-    fill('#222');
+    switch(this.type) {
+      case 'circle':
+        renderPath([['arc', x + 2, y + 2, 13, 0, Math.PI * 2]]);
+        fill('#222');
 
-    renderPath([['arc', x, y, this.size, 0, Math.PI * 2]]);
-    fill('rgb(' + Math.round(255 * (this.life / this.size)) + ',' + Math.round(255 * ((this.size - this.life) / this.size)) + ',' + 0x22 + ')');
+        renderPath([['arc', x, y, this.size, 0, Math.PI * 2]]);
+        fill('rgb(' + Math.round(255 * (this.life / this.size)) + ',' + Math.round(255 * ((this.size - this.life) / this.size)) + ',' + 0x22 + ')');
+        break;
+    }
   }
 }
