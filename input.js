3322b53f5bd494d9992cb30065299deecae97635
Peter Wood <pwood@sankyinc.com>
2017-01-30 10:51:46 -0500

    Update input.render to use global render functions

--- input.js
+++ input.js
@@ -39,72 +39,64 @@ gInput = {
   },
   isActive: function(command) { return typeof this[command] === 'string' && this[this[command]] === 1 },
   setMousePos: function(event) { this.mouseX = event.layerX; this.mouseY = event.layerY },
-  render: (function(context) {
-    function render() {
-      var rad = this.rotateCursor ? this.rad : gPlayer.rad + Math.PI;
-      if (this.rotateCursor) this.rad += Math.PI / 512;
+  render: function() {
+    var rad = this.rotateCursor ? this.rad : gPlayer.rad + Math.PI;
+    if (this.rotateCursor) this.rad += Math.PI / 512;
 
-      context.beginPath();
-      switch (this.cursor) {
-        case 'crosshairs':
-          context.moveTo(this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (16 * Math.cos(rad)), this.mouseY + (16 * Math.sin(rad)));
-          context.moveTo(this.mouseX + (4 * Math.cos(rad + (Math.PI / 2))), this.mouseY + (4 * Math.sin(rad + (Math.PI / 2))));
-          context.lineTo(this.mouseX + (16 * Math.cos(rad + (Math.PI / 2))), this.mouseY + (16 * Math.sin(rad + (Math.PI / 2))));
-          context.moveTo(this.mouseX + (4 * Math.cos(rad + Math.PI)), this.mouseY + (4 * Math.sin(rad + Math.PI)));
-          context.lineTo(this.mouseX + (16 * Math.cos(rad + Math.PI)), this.mouseY + (16 * Math.sin(rad + Math.PI)));
-          context.moveTo(this.mouseX + (4 * Math.cos(rad - (Math.PI / 2))), this.mouseY - (4 * Math.sin(rad + (Math.PI / 2))));
-          context.lineTo(this.mouseX + (16 * Math.cos(rad - (Math.PI / 2))), this.mouseY - (16 * Math.sin(rad + (Math.PI / 2))));
-          context.moveTo(this.mouseX + (12 * Math.cos(rad + (Math.PI / 16))), this.mouseY + (12 * Math.sin(rad + (Math.PI / 16))));
-          context.arc(this.mouseX, this.mouseY, 12, rad + (Math.PI / 16), rad + (Math.PI * 7 / 16));
-          context.moveTo(this.mouseX + (12 * Math.cos(rad + (Math.PI * 9 / 16))), this.mouseY + (12 * Math.sin(rad + (Math.PI * 9 / 16))));
-          context.arc(this.mouseX, this.mouseY, 12, rad + (Math.PI * 9 / 16), rad + (Math.PI * 15 / 16));
-          context.moveTo(this.mouseX + (12 * Math.cos(rad + (Math.PI * 17 / 16))), this.mouseY + (12 * Math.sin(rad + (Math.PI * 17 / 16))));
-          context.arc(this.mouseX, this.mouseY, 12, rad + (Math.PI * 17 / 16), rad - (Math.PI * 9 / 16));
-          context.moveTo(this.mouseX + (12 * Math.cos(rad - (Math.PI * 7 / 16))), this.mouseY + (12 * Math.sin(rad - (Math.PI * 7 / 16))));
-          context.arc(this.mouseX, this.mouseY, 12, rad - (Math.PI * 7 / 16), rad - (Math.PI / 16));
-          context.strokeStyle = '#2D2';
-          context.lineWidth = 2;
-          context.stroke();
-          context.closePath();
-          context.lineWidth = 1;
-          context.strokeRect(this.mouseX - 0.5, this.mouseY - 0.5, 1, 1);
-          break;
-        case 'triangle':
-          context.moveTo(this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (18 * Math.cos(rad - (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad - (Math.PI / 8))));
-          context.lineTo(this.mouseX + (14 * Math.cos(rad)), this.mouseY + (14 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (18 * Math.cos(rad + (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad + (Math.PI / 8))));
-          context.strokeStyle = 'rgba(0,0,0,0.6)';
-          context.lineWidth = 2;
-          context.closePath();
-          context.stroke();
-          context.fillStyle = "#DD2";
-          context.fill();
-          context.fillStyle = "#2D2";
-          context.beginPath();
-          rad += Math.PI * 2 / 3;
-          context.moveTo(this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (18 * Math.cos(rad - (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad - (Math.PI / 8))));
-          context.lineTo(this.mouseX + (14 * Math.cos(rad)), this.mouseY + (14 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (18 * Math.cos(rad + (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad + (Math.PI / 8))));
-          context.closePath();
-          context.stroke();
-          context.fill();
-          rad += Math.PI * 2 / 3;
-          context.moveTo(this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (18 * Math.cos(rad - (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad - (Math.PI / 8))));
-          context.lineTo(this.mouseX + (14 * Math.cos(rad)), this.mouseY + (14 * Math.sin(rad)));
-          context.lineTo(this.mouseX + (18 * Math.cos(rad + (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad + (Math.PI / 8))));
-          context.closePath();
-          context.stroke();
-          context.fillStyle = "#2D2";
-          context.fill();
-          break;
-      }
+    switch (this.cursor) {
+      case 'crosshairs':
+        renderPath([
+          ['moveTo', this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (16 * Math.cos(rad)), this.mouseY + (16 * Math.sin(rad))],
+          ['moveTo', this.mouseX + (4 * Math.cos(rad + (Math.PI / 2))), this.mouseY + (4 * Math.sin(rad + (Math.PI / 2)))],
+          ['lineTo', this.mouseX + (16 * Math.cos(rad + (Math.PI / 2))), this.mouseY + (16 * Math.sin(rad + (Math.PI / 2)))],
+          ['moveTo', this.mouseX + (4 * Math.cos(rad + Math.PI)), this.mouseY + (4 * Math.sin(rad + Math.PI))],
+          ['lineTo', this.mouseX + (16 * Math.cos(rad + Math.PI)), this.mouseY + (16 * Math.sin(rad + Math.PI))],
+          ['moveTo', this.mouseX + (4 * Math.cos(rad - (Math.PI / 2))), this.mouseY - (4 * Math.sin(rad + (Math.PI / 2)))],
+          ['lineTo', this.mouseX + (16 * Math.cos(rad - (Math.PI / 2))), this.mouseY - (16 * Math.sin(rad + (Math.PI / 2)))],
+          ['moveTo', this.mouseX + (12 * Math.cos(rad + (Math.PI / 16))), this.mouseY + (12 * Math.sin(rad + (Math.PI / 16)))],
+          ['arc', this.mouseX, this.mouseY, 12, rad + (Math.PI / 16), rad + (Math.PI * 7 / 16)],
+          ['moveTo', this.mouseX + (12 * Math.cos(rad + (Math.PI * 9 / 16))), this.mouseY + (12 * Math.sin(rad + (Math.PI * 9 / 16)))],
+          ['arc', this.mouseX, this.mouseY, 12, rad + (Math.PI * 9 / 16), rad + (Math.PI * 15 / 16)],
+          ['moveTo', this.mouseX + (12 * Math.cos(rad + (Math.PI * 17 / 16))), this.mouseY + (12 * Math.sin(rad + (Math.PI * 17 / 16)))],
+          ['arc', this.mouseX, this.mouseY, 12, rad + (Math.PI * 17 / 16), rad - (Math.PI * 9 / 16)],
+          ['moveTo', this.mouseX + (12 * Math.cos(rad - (Math.PI * 7 / 16))), this.mouseY + (12 * Math.sin(rad - (Math.PI * 7 / 16)))],
+          ['arc', this.mouseX, this.mouseY, 12, rad - (Math.PI * 7 / 16), rad - (Math.PI / 16)],
+        ]);
+
+        stroke('#2D2', 2);
+        strokeRect(this.mouseX - 0.5, this.mouseY - 0.5, 1, 1, 1);
+        break;
+      case 'triangle':
+        renderPath([
+          ['moveTo', this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (18 * Math.cos(rad - (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad - (Math.PI / 8)))],
+          ['lineTo', this.mouseX + (14 * Math.cos(rad)), this.mouseY + (14 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (18 * Math.cos(rad + (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad + (Math.PI / 8)))],
+        ], true);
+        stroke('rgba(0,0,0,0.6)', 2);
+        fill('#DD2');
+        rad += Math.PI * 2 / 3;
+        renderPath([
+          ['moveTo', this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (18 * Math.cos(rad - (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad - (Math.PI / 8)))],
+          ['lineTo', this.mouseX + (14 * Math.cos(rad)), this.mouseY + (14 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (18 * Math.cos(rad + (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad + (Math.PI / 8)))],
+        ], true);
+        stroke();
+        fill("#2D2");
+        rad += Math.PI * 2 / 3;
+        renderPath([
+          ['moveTo', this.mouseX + (4 * Math.cos(rad)), this.mouseY + (4 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (18 * Math.cos(rad - (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad - (Math.PI / 8)))],
+          ['lineTo', this.mouseX + (14 * Math.cos(rad)), this.mouseY + (14 * Math.sin(rad))],
+          ['lineTo', this.mouseX + (18 * Math.cos(rad + (Math.PI / 8))), this.mouseY + (18 * Math.sin(rad + (Math.PI / 8)))],
+        ], true);
+        stroke();
+        fill();
+        break;
     }
-    return render;
-  })(gRenderer.context)
+  }
 }
 
 function handleKeyDown(event) {
